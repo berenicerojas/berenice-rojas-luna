4cea360f41064236a7857064824d1ce1f05d8d34
berenicerojas <berenicerojas.nyc@gmail.com>
2025-09-27 13:53:04 -0400

    revisions added for lesson 11!

--- js/index.js
+++ js/index.js
@@ -12,19 +12,18 @@ var thisYear = today.getFullYear();
 document.querySelector('footer');
 //Copyright section
 const copyright = document.createElement('p');
-body.appendChild(copyright);
 //Included the copyright name 
-copyright.innerHTML = 'BRG' + '©' + thisYear;
+copyright.innerHTML = '©' + 'BRG' + thisYear;
 footer.appendChild(copyright);
 
 // -------------------- Skills --------------------
 
 const skills = ['JavaScript', 'HTML', 'CSS', 'Adobe Photoshop', 'GitHub'];
-const skillsSection = document.querySelector('#skills');
+const skillsSection = document.querySelector('#Skills');
 const skillsList = skillsSection.querySelector('ul');
 
-for (let i = 0; i < skill.length; i++) {
-    const skill = document.createElement('ul');
+for (let i = 0; i < skills.length; i++) {
+    const skill = document.createElement('li');
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
